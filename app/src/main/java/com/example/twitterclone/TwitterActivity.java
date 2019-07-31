f5f46fb673e289d47c94c2d03966e81edebc1c90
Your Name <you@example.com>
2019-07-31 14:41:20 +0530

    1 - SignUp, LogIn & Logout 2 - Fetching List of Users & displaying them using ListView 3 - Saving, Fetching & Saving followed and Unfollowed Users

---
+++ app/src/main/java/com/example/twitterclone/TwitterActivity.java
@@ -0,0 +1,126 @@
+package com.example.twitterclone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.FindCallback;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.dmoral.toasty.Toasty;
+
+public class TwitterActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView listView;
+    private ArrayList<String> tUsers;
+    private ArrayAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_twitter );
+
+        Toasty.success( this,"Welcome " + ParseUser.getCurrentUser().getUsername(),Toasty.LENGTH_SHORT ).show();
+
+        listView = findViewById(R.id.listView);
+        tUsers = new ArrayList<>();
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, tUsers);
+        listView.setChoiceMode( AbsListView.CHOICE_MODE_MULTIPLE);
+        listView.setOnItemClickListener(this);
+
+        try {
+            ParseQuery<ParseUser> query = ParseUser.getQuery();
+            query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+            query.findInBackground(new FindCallback<ParseUser>() {
+                @Override
+                public void done(List<ParseUser> objects, ParseException e) {
+                    if (objects.size() > 0 && e == null) {
+                        for (ParseUser twitterUser : objects) {
+                            tUsers.add(twitterUser.getUsername());
+                        }
+                        listView.setAdapter(adapter);
+                        for (String twitterUser : tUsers) {
+                            if (ParseUser.getCurrentUser().getList("fanOf") != null) {
+                                if (ParseUser.getCurrentUser().getList("fanOf").contains(twitterUser)) {
+                                    listView.setItemChecked(tUsers.indexOf(twitterUser), true);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.my_menu, menu );
+        return super.onCreateOptionsMenu( menu );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_item :
+                ParseUser.getCurrentUser().logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Intent intent = new Intent(TwitterActivity.this, SignUpActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                break;
+            case R.id.sendTweetItem:
+
+                Intent intent = new Intent(TwitterActivity.this, SendTweetActivity.class);
+                startActivity(intent);
+
+                break;
+        }
+        return super.onOptionsItemSelected( item );
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        CheckedTextView checkedTextView =  (CheckedTextView) view;
+        if (checkedTextView.isChecked()) {
+            Toasty.info( TwitterActivity.this, tUsers.get( position ) + " is now followed", Toasty.LENGTH_SHORT ).show();
+            ParseUser.getCurrentUser().add("fanOf", tUsers.get(position));
+        } else {
+            ParseUser.getCurrentUser().getList("fanOf").remove(tUsers.get(position));
+            List currentUserFanOfList = ParseUser.getCurrentUser().getList("fanOf");
+            ParseUser.getCurrentUser().remove("fanOf");
+            ParseUser.getCurrentUser().put("fanOf", currentUserFanOfList);
+            Toasty.info( TwitterActivity.this, tUsers.get( position ) + " is now Unfollowed", Toasty.LENGTH_SHORT ).show();
+        }
+
+        ParseUser.getCurrentUser().saveInBackground( new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toasty.success( TwitterActivity.this, "Saved", Toasty.LENGTH_SHORT ).show();
+                }
+            }
+        } );
+    }
+}
